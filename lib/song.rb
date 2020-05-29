c957cea527b752637350accd17164d918c037f4b
alexisteh <wenhui.teh23@gmail.com>
2020-05-29 18:53:56 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -6,31 +6,37 @@ class Song
 
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
+  # def initialize
+  #   self.class.all << self
+  # end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  extend Findable 
+
+  # def self.find_by_name(name)
+  #   @@songs.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@songs
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+  extend Memorable::ClassMethods 
+  include Memorable::InstanceMethods 
 
-  def self.count
-    self.all.count
-  end
+  # def self.reset_all
+  #   self.all.clear
+  # end
+
+  # def self.count
+  #   self.all.count
+  # end
 
   def artist=(artist)
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  include Paramable 
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
