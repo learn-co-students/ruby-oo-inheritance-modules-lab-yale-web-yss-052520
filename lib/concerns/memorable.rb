adb515ac34ad632f3329f48817f35a7bc35be1e0
mlkelly <maurak87@gmail.com>
2020-05-30 00:12:50 -0400

    Done.

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,17 @@
+module Memorable #class methods
+    module ClassMethods
+        def reset_all
+            self.all.clear
+        end
+
+        def count
+            self.all.count
+        end
+    end
+
+    module InstanceMethods
+        def initialize
+            self.class.all << self
+        end
+    end
+end
