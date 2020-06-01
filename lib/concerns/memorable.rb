4d580b38b47b98e3b2aeb9f6bd9e30e7fde69a27
ev275 <enrique.valencia@yale.edu>
2020-06-01 12:05:25 -0500

    Done.

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,11 @@
+module Memorable
+    module ClassInstances
+        def reset_all
+            self.all.clear
+        end
+      
+        def count
+            self.all.count
+        end
+    end
+end
